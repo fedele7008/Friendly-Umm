974880b62d6a5294497d25b486de1a6b1217c8de
John Yoon <fedelejohn7008@gmail.com>
2023-11-19 23:46:47 -0500

    clion support and reset the project

--- apps/FriendlyUmmCompiler/main.cpp
+++ apps/FriendlyUmmCompiler/main.cpp
@@ -1,30 +1,9 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <sstream>
-#include <utility>
-#include <map>
-#include <algorithm>
 #include "scanner.h"
 
-using scanner::Token;
-using scanner::scan;
-using scanner::ScanningFailureException;
+#include <spdlog/spdlog.h>
 
-int main(int argc, char **argv) {
-    std::string line;
-    try {
-    while (getline(std::cin, line)) {
-      std::vector<Token> tokenLine = scan(line);
-
-      for (auto &token : tokenLine) {
-        std::cout << token << '\n';
-      }
-    }
-  } catch (ScanningFailureException &f) {
-    std::cerr << f.what() << std::endl;
-    return 1;
-  }
-  
-  return 0;
+int main(int argc, char *argv[])
+{
+    spdlog::set_level(spdlog::level::trace);
+    auto scanner = scanner::CreateScanner();
 }
--- modules/Scanner/include/scanner.h
+++ modules/Scanner/include/scanner.h
@@ -1,81 +1,20 @@
-#ifndef __SCANNER_H__
-#define __SCANNER_H__
+#ifndef FEDELE7008_SCANNER_SCANNER_H
+#define FEDELE7008_SCANNER_SCANNER_H
 
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <utility>
-#include <map>
-#include <vector>
+#include <memory>
 
 namespace scanner {
-    using std::vector;
-    using std::string;
-    using std::ostream;
 
-    class Token;
-
-    vector<Token> scan(const string &input);
-    ostream &operator<<(ostream &out, const Token &token);
-
-    class Token {
-        public:
-        enum Type {
-            SPACE = 0,
-            TAB,
-            NEWLINE,
-            COMMENT,
-            BEGF,
-            ENDF,
-            ID,
-            NUM,
-            LPAREN,
-            RPAREN,
-            LBRACE,
-            RBRACE,
-            RETURN,
-            IF,
-            ELSE,
-            WHILE,
-            PRINT,
-            PRINTLN,
-            INPUT,
-            MAIN,
-            ASSIGN,
-            INT,
-            CHAR,
-            EQ,
-            NE,
-            PLUS,
-            MINUS,
-            STAR,
-            SLASH,
-            COMMA,
-            SEMI,
-            LBRACK,
-            RBRACK,
-            STR,
-        };
-
-        private:
-        Type type;
-        string lexeme;
-
-        public:
-        Token(Type type, string lexeme);
-
-        Type getType() const;
-        const string &getLexeme() const;
+    class Scanner {
+    private:
+        uint32_t id_counter;
+        uint32_t m_id;
+    public:
+        Scanner();
+        virtual ~Scanner();
     };
 
-    class ScanningFailureException {
-        string msg;
-
-        public:
-        ScanningFailureException(const string &msg);
-
-        const string &what() const;
-    };
+    std::shared_ptr<Scanner> CreateScanner();
 }
 
 #endif
