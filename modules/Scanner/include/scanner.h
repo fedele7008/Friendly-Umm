79a7b9125624e0238609f1c9d2df380f43e2f66f
John Yoon <fedelejohn7008@gmail.com>
2023-11-22 00:44:55 -0500

    dependency update

--- modules/Scanner/include/scanner.h
+++ modules/Scanner/include/scanner.h
@@ -1,20 +1,14 @@
 #ifndef FEDELE7008_SCANNER_SCANNER_H
 #define FEDELE7008_SCANNER_SCANNER_H
 
-#include <memory>
-
 namespace scanner {
-
     class Scanner {
-    private:
-        uint32_t id_counter;
-        uint32_t m_id;
     public:
         Scanner();
         virtual ~Scanner();
     };
 
-    std::shared_ptr<Scanner> CreateScanner();
+    Scanner *CreateScanner();
 }
 
 #endif
