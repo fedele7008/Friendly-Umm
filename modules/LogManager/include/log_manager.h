4161529c20861a20043cf8e6043f96a93c5a152d
John Yoon <fedelejohn7008@gmail.com>
2023-12-17 22:30:28 -0500

    log manager implementation

--- modules/LogManager/include/log_manager.h
+++ modules/LogManager/include/log_manager.h
@@ -4,77 +4,101 @@
 #include <memory>
 #include <string>
 #include <filesystem>
+#include <optional>
 
 #include <spdlog/spdlog.h>
 
 namespace service {
-using std::shared_ptr;
-using std::string;
-using std::filesystem::path;
-using spdlog::logger;
-
-/**
- * @brief Log management service.
- */
-class LogManager {
-private:
-    /**
-     * Shared logger pointer.
-     */
-    static shared_ptr<logger> m_logger;
+    using std::shared_ptr;
+    using std::string;
+    using std::filesystem::path;
+    using std::optional;
+    using spdlog::logger;
 
     /**
-     * Private constructor to prevent instantiation.
-     * @brief private constructor.
+     * @brief Log management service.
      */
-    explicit LogManager() = default;
+    class LogManager {
+    private:
+        /**
+         * Shared logger pointer.
+         */
+        shared_ptr<logger> m_logger;
 
-public:
-    /**
-     * Public destructor with default implementation.
-     * @brief Default destructor.
-     */
-    virtual ~LogManager() = default;
+        /**
+         * check if logger has been initialized.
+         */
+        bool m_initialized = false;
 
-    /**
-     * Disable copy constructor.
-     * @brief Disable copy constructor.
-     */
-    LogManager(const LogManager&) = delete;
+        /**
+         * logger name.
+         */
+        string m_logger_name;
 
-    /**
-     * Disable assignment operator.
-     * @brief Disable assignment operator.
-     */
-    LogManager& operator=(const LogManager&) = delete;
+        /**
+         * Private constructor to prevent instantiation.
+         * @brief private constructor.
+         */
+        explicit LogManager() = default;
 
-    /**
-     * Disable move constructor.
-     * @brief Disable move constructor.
-     */
-    LogManager(LogManager&&) = delete;
+    public:
+        /**
+         * Public destructor with default implementation.
+         * @brief Default destructor.
+         */
+        virtual ~LogManager() = default;
 
-    /**
-     * Disable move assignment operator.
-     * @brief Disable move assignment operator.
-     */
-    LogManager& operator=(LogManager&&) = delete;
+        /**
+         * Disable copy constructor.
+         * @brief Disable copy constructor.
+         */
+        LogManager(const LogManager &) = delete;
 
-    /**
-     * Gets the singleton instance of the LogManager.
-     * @brief Returns the singleton instance of the LogManager.
-     * @return The singleton instance of the LogManager.
-     */
-    static LogManager& getInstance();
+        /**
+         * Disable assignment operator.
+         * @brief Disable assignment operator.
+         */
+        LogManager &operator=(const LogManager &) = delete;
 
-    /**
-     * Initializes the logger.
-     * @brief Initializes the logger.
-     * @param logPath The path to the log file.
-     */
-    void initialize(path &logPath);
+        /**
+         * Disable move constructor.
+         * @brief Disable move constructor.
+         */
+        LogManager(LogManager &&) = delete;
 
-};
+        /**
+         * Disable move assignment operator.
+         * @brief Disable move assignment operator.
+         */
+        LogManager &operator=(LogManager &&) = delete;
+
+        /**
+         * Gets the singleton instance of the LogManager.
+         * @brief Returns the singleton instance of the LogManager.
+         * @return The singleton instance of the LogManager.
+         */
+        static LogManager &getInstance() noexcept;
+
+        /**
+         * Initializes the logger.
+         * @brief Initializes the logger.
+         * @param logPath The path to the log file.
+         */
+        void initialize(const string &name, const optional<path> &logPath = {});
+
+        /**
+         * Un-initializes the logger.
+         * @brief Un-initializes the logger.
+         */
+        void uninitialize();
+
+        /**
+         * Gets the logger.
+         * @brief Gets the logger.
+         * @return The logger.
+         */
+        shared_ptr<logger> getLogger();
+    };
 } // namespace service
 
 #endif // FEDELE7008_LOG_MANAGER_LOG_MANAGER_H_
